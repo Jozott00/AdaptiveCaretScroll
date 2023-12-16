e159907dc5fc04a8ecc8b58538e004c6b07f7091
Johannes Zottele <official@johannes-zottele.at>
2023-12-16 10:54:49 +0100

    Update settings to build with 2022.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,7 +14,7 @@ repositories {
 // Configure Gradle IntelliJ Plugin
 // Read more: https://plugins.jetbrains.com/docs/intellij/tools-gradle-intellij-plugin.html
 intellij {
-    version.set("2023.3")
+    version.set("2022.2")
     type.set("IC") // Target IDE Platform
 
     plugins.set(listOf(/* Plugin Dependencies */))
