03ac617333e3d0dfe6074fee840b3e64b8e8c27c
Johannes Zottele <official@johannes-zottele.at>
2023-12-16 11:04:50 +0100

    Update settings to support 233.*

--- build.gradle.kts
+++ build.gradle.kts
@@ -32,7 +32,7 @@ tasks {
 
     patchPluginXml {
         sinceBuild.set("222")
-//        untilBuild.set("233.*") // support all future versions
+        untilBuild.set("233.*")
     }
 
 //    signPlugin {
