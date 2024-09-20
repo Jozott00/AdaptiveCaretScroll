ac0260bda3641c6b700c6cf498f52a01773e1d59
Johannes Zottele <johannes.zottele@tuwien.ac.at>
2024-09-20 13:45:40 +0200

    Support IntelliJ Platform 2024.2 and 2024.3

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,7 +17,7 @@ repositories {
 // Configure Gradle IntelliJ Plugin
 // Read more: https://plugins.jetbrains.com/docs/intellij/tools-gradle-intellij-plugin.html
 intellij {
-    version.set("2022.2")
+    version.set("2024.2")
     type.set("IC") // Target IDE Platform
 
     plugins.set(listOf(/* Plugin Dependencies */))
@@ -35,8 +35,8 @@ tasks {
 
     patchPluginXml {
         version.set(System.getenv("PLUGIN_VERSION").or("1.1.3"))
-        sinceBuild.set("222")
-        untilBuild.set("241.*")
+        sinceBuild.set("242.2")
+        untilBuild.set("243.*")
     }
 
 //    signPlugin {
